dff92f9406f3cac7d574f5461b6bee3b9157da2f
Brian Harrold <brianharrold6@gmail.com>
2025-06-05 09:49:15 -0400

    Updating the package

--- R/get_COHORTS.R
+++ R/get_COHORTS.R
@@ -1,3 +1,20 @@
+#' Takes a long dataset and separates it into a list of cohort datasets by grade level and year
+#' 
+#' @title Get Cohort Data from Long Dataset
+#' @description Returns "Hello, World!" in various languages using the Hello_World dataset
+#' 
+#' @param DATASET a data frame in long formate with columns for GRADE and YEAR
+#' 
+#' @return A list containing cohort datasets by grade level and year
+#' @export
+#' 
+#' @examples
+#' library(SGPdata)
+#' get_COHORT(DATASET = sgpDATA_LONG[sgpDATA_LONG$CONTENT_AREA == "MATHEMATICS", ])
+#' 
+
+
+
 get_COHORTS = function(DATASET) {
   COHORTS = split(DATASET, list(DATASET$GRADE, DATASET$YEAR), drop = TRUE)
   return(COHORTS)
--- R/hello_WORLD.R
+++ R/hello_WORLD.R
@@ -1,3 +1,14 @@
+#' Hello World in English
+#' 
+#' @title Simple Hello World function
+#' @description Returns "Hello, World!" in English
+#' 
+#' @export
+#' 
+#' @examples
+#' hello_WORLD()  # Returns English greeting
+#' 
+
 hello_WORLD = function() {
-  print("Hello World")
+  print("Hello World!")
 }
