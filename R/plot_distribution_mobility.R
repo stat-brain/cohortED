3567bbbde4d922083a410e03a5184daf0eece935
Brian Harrold <brianharrold6@gmail.com>
2025-06-12 15:56:20 -0400

    Update to website

---
+++ R/plot_distribution_mobility.R
@@ -0,0 +1,18 @@
+#' Plot Mobility Distribution over Grade Levels
+#' 
+#' @title Plot Mobility Distribution
+#' @description Creates an Alluvial Diagram depicting Student Mobility from previous year
+#' 
+#' @param dataset A data frame that includes student grade level and academic year
+#' @param year The academic year to begin evaluating with (only required if not already used make_mobility)
+#' @param print_table Prints a contingency table of indicators associated with student mobility (default: TRUE)
+#' 
+#' 
+#' @return The Distribution of Student Mobility for a given Year for all Grade levels
+#' @import ggplot2
+#' @export
+#' 
+
+plot_distribution_mobility = function(dataset, year) {
+  
+}
